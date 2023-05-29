4aad3f01be15f7c424f3b6b053b04d87f270766c
Miftahul Huda <hudamiftahul999@gmail.com>
2023-05-29 10:46:52 +0700

    Mengubah Kustomisasi Halaman Fragment - Menambahkan Fragment Home, Chat, Info Jurusan, Teman, dan Profile - Dependency Fragment & Navigation - Menambahkan menu pada bottom navigation

--- app/src/main/java/com/capstone/arahku/ui/friend/FriendFragment.kt
+++ app/src/main/java/com/capstone/arahku/ui/friend/FriendFragment.kt
@@ -1,4 +1,4 @@
-package com.capstone.arahku.ui.kating
+package com.capstone.arahku.ui.friend
 
 import android.os.Bundle
 import androidx.fragment.app.Fragment
@@ -14,10 +14,10 @@ private const val ARG_PARAM2 = "param2"
 
 /**
  * A simple [Fragment] subclass.
- * Use the [KatingFragment.newInstance] factory method to
+ * Use the [FriendFragment.newInstance] factory method to
  * create an instance of this fragment.
  */
-class KatingFragment : Fragment() {
+class FriendFragment : Fragment() {
     // TODO: Rename and change types of parameters
     private var param1: String? = null
     private var param2: String? = null
@@ -35,7 +35,7 @@ class KatingFragment : Fragment() {
         savedInstanceState: Bundle?,
     ): View? {
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_kating, container, false)
+        return inflater.inflate(R.layout.fragment_friend, container, false)
     }
 
     companion object {
@@ -45,12 +45,12 @@ class KatingFragment : Fragment() {
          *
          * @param param1 Parameter 1.
          * @param param2 Parameter 2.
-         * @return A new instance of fragment KatingFragment.
+         * @return A new instance of fragment FriendFragment.
          */
         // TODO: Rename and change types and number of parameters
         @JvmStatic
         fun newInstance(param1: String, param2: String) =
-            KatingFragment().apply {
+            FriendFragment().apply {
                 arguments = Bundle().apply {
                     putString(ARG_PARAM1, param1)
                     putString(ARG_PARAM2, param2)
