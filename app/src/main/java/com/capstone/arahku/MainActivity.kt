4db037ea8a13d50944f968daefca84474bce89f3
Miftahul Huda <hudamiftahul999@gmail.com>
2023-06-01 11:35:51 +0700

    Mengubah Kustomisasi Halaman Home dan Navigasi - Menambahkan resource image dalam format webp - Menghapus fragment friend dan info jurusan - Menambahkan fragment notification - Memperbarui format icon pada bottom navigation

--- app/src/main/java/com/capstone/arahku/MainActivity.kt
+++ app/src/main/java/com/capstone/arahku/MainActivity.kt
@@ -1,8 +1,8 @@
 package com.capstone.arahku
 
 import android.content.Intent
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
 import androidx.fragment.app.Fragment
 import androidx.lifecycle.ViewModelProvider
 import com.capstone.arahku.databinding.ActivityMainBinding
@@ -10,9 +10,8 @@ import com.capstone.arahku.model.UserPreference
 import com.capstone.arahku.model.dataStore
 import com.capstone.arahku.ui.LandingPageActivity
 import com.capstone.arahku.ui.chat.ChatFragment
-import com.capstone.arahku.ui.friend.FriendFragment
 import com.capstone.arahku.ui.home.HomeFragment
-import com.capstone.arahku.ui.info_jurusan.InfoJurusanFragment
+import com.capstone.arahku.ui.notification.NotificationFragment
 import com.capstone.arahku.ui.profile.ProfileFragment
 import com.capstone.arahku.viewmodel.LoginViewModel
 import com.capstone.arahku.viewmodel.ViewModelFactory
@@ -29,23 +28,21 @@ class MainActivity : AppCompatActivity() {
         binding = ActivityMainBinding.inflate(layoutInflater)
         setContentView(binding.root)
         loadFragment(HomeFragment())
-        bottomNav = binding.navView as BottomNavigationView
+        supportActionBar?.hide()
+
+        bottomNav = binding.navView
         bottomNav.setOnItemSelectedListener {
             when(it.itemId){
                 R.id.navigation_home -> {
                     loadFragment(HomeFragment())
                     true
                 }
+                R.id.navigation_notification -> {
+                    loadFragment(NotificationFragment())
+                    true
+                }
                 R.id.navigation_chat -> {
                     loadFragment(ChatFragment())
-                    true
-                }
-                R.id.navigation_info_jurusan -> {
-                    loadFragment(InfoJurusanFragment())
-                    true
-                }
-                R.id.navigation_teman -> {
-                    loadFragment(FriendFragment())
                     true
                 }
                 R.id.navigation_profile -> {
---
+++ app/src/main/java/com/capstone/arahku/ui/notification/NotificationFragment.kt
@@ -0,0 +1,13 @@
+package com.capstone.arahku.ui.notification
+
+import android.os.Bundle
+import android.view.View
+import androidx.fragment.app.Fragment
+
+class NotificationFragment : Fragment() {
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+    }
+
+}
