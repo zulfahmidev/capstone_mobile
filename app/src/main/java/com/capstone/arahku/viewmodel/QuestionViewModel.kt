3efe9dae53c805d0bf2d1d19e97d4f139bbf4d04
Miftahul Huda <hudamiftahul999@gmail.com>
2023-06-12 09:54:26 +0700

    Memperbaiki kode xml pada halaman register

--- app/src/main/java/com/capstone/arahku/viewmodel/QuestionViewModel.kt
+++ app/src/main/java/com/capstone/arahku/viewmodel/QuestionViewModel.kt
@@ -1,7 +1,6 @@
 package com.capstone.arahku.viewmodel
 
 import android.util.Log
-import android.widget.Toast
 import androidx.lifecycle.LiveData
 import androidx.lifecycle.MutableLiveData
 import androidx.lifecycle.ViewModel
