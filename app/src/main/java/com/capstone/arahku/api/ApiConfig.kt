e552021ea4b4a5914eb8bbdb4e803dfdeace094e
Miftahul Huda <hudamiftahul999@gmail.com>
2023-06-14 14:19:12 +0700

    Update API Config

--- app/src/main/java/com/capstone/arahku/api/ApiConfig.kt
+++ app/src/main/java/com/capstone/arahku/api/ApiConfig.kt
@@ -18,7 +18,7 @@ class ApiConfig {
                 .addInterceptor(loggingInterceptor)
                 .build()
             val retrofit = Retrofit.Builder()
-                .baseUrl("http://api.sepol.xyz/")
+                .baseUrl("http://sepol.xyz/")
                 .addConverterFactory(GsonConverterFactory.create())
                 .client(client)
                 .build()
